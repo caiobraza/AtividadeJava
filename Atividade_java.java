cdbad4be43c96ab834bbe95f79337d00c1e69522
caiobraza <caio.lg.g2@gmail.com>
2023-02-19 20:18:01 -0300

    first commit

---
+++ Atividade_java.java
@@ -0,0 +1,19 @@
+package aulajava;
+
+public class Atividade_java {
+
+	public static void main(String[] args) {
+		/*
+		   Atividade: Crie uma aplicação para descobrir
+		   e mostrar a idade de alguém. 
+		*/
+		
+		int anoAtual = 2023;
+		int anoDeNascimento = 1998;
+		int idadeAtual = anoAtual - anoDeNascimento;
+		
+		System.out.println("A idade atual é:" + idadeAtual);
+
+	}
+
+}
